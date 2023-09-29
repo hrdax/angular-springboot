ab3ea6d0e660681ade382132839afcb9ada19951
hrdax <juanpa_200018@hotmail.com>
2023-09-29 13:03:40 -0300

    usando findallbycountry rest

--- BackEndPersona/src/main/java/sys/jp/persona/rest/EstadoRest.java
+++ BackEndPersona/src/main/java/sys/jp/persona/rest/EstadoRest.java
@@ -1,5 +1,25 @@
 package sys.jp.persona.rest;
 
-public class EstadoRest {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import sys.jp.persona.model.Estado;
+import sys.jp.persona.service.EstadoService;
+
+@RestController
+@RequestMapping("/estado/")
+public class EstadoRest {
+	
+	@Autowired
+	private EstadoService estadoService;
+	
+	@GetMapping ("{id}")
+	private ResponseEntity<List<Estado>> getAllEstadosByPais (@PathVariable("id") int idPais){
+		return ResponseEntity.ok(estadoService.findAllByCountry(idPais));
+	}
 }

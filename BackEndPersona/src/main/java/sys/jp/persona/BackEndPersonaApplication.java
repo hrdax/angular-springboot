0cc37839f20e55273221292eb4a78d0e998bf971
hrdax <juanpa_200018@hotmail.com>
2023-09-29 08:00:25 -0300

    first commit backend persona

---
+++ BackEndPersona/src/main/java/sys/jp/persona/BackEndPersonaApplication.java
@@ -0,0 +1,13 @@
+package sys.jp.persona;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndPersonaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndPersonaApplication.class, args);
+	}
+
+}
---
+++ BackEndPersona/src/test/java/sys/jp/persona/BackEndPersonaApplicationTests.java
@@ -0,0 +1,13 @@
+package sys.jp.persona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndPersonaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

639130bbe94580f336e4d7d7fa9069606a970636
hrdax <juanpa_200018@hotmail.com>
2023-09-29 08:29:59 -0300

    relacion manytoone join column

--- BackEndPersona/src/main/java/sys/jp/persona/model/Persona.java
+++ BackEndPersona/src/main/java/sys/jp/persona/model/Persona.java
@@ -1,9 +1,12 @@
 package sys.jp.persona.model;
 
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -16,6 +19,9 @@ public class Persona {
 	private String apellido;
 	private int edad;
 	
+	@ManyToOne
+	@JoinColumn (name = "id_pais")
 	private Pais pais;
+	@JoinColumn (name = "id_estado")
 	private Estado estado;
 }

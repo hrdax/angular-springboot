0be16a6b198466c3088c26284b7ac2c7b544068a
hrdax <juanpa_200018@hotmail.com>
2023-09-29 16:43:31 -0300

    getall paises api

--- front_persona/src/app/services/paises/paises.service.ts
+++ front_persona/src/app/services/paises/paises.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaisesService {
 
-  constructor() { }
+  private API_SERVER = "http://localhost:8081/paises/"
+
+  constructor(private httpClient: HttpClient) { }
+
+  public getAllPaises(): Observable<any> {
+    return this.httpClient.get(this.API_SERVER);
+  }
+
+
 }

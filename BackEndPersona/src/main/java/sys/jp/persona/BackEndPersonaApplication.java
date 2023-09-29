592e4e56fd20f1f7a6c329d23b1734c5174e1de4
hrdax <juanpa_200018@hotmail.com>
2023-09-29 16:43:17 -0300

    solucionado problema cors

--- BackEndPersona/src/main/java/sys/jp/persona/BackEndPersonaApplication.java
+++ BackEndPersona/src/main/java/sys/jp/persona/BackEndPersonaApplication.java
@@ -2,6 +2,8 @@ package sys.jp.persona;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @SpringBootApplication
 public class BackEndPersonaApplication {
@@ -9,5 +11,14 @@ public class BackEndPersonaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackEndPersonaApplication.class, args);
 	}
-
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("*").allowedHeaders("*");
+			}
+		}
+	}
 }

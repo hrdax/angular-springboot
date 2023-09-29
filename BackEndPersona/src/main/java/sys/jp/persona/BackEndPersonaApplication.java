05219538aa5807261c661a268010d8988e18cd37
hrdax <juanpa_200018@hotmail.com>
2023-09-29 16:56:21 -0300

    fix punto y coma, import

--- BackEndPersona/src/main/java/sys/jp/persona/BackEndPersonaApplication.java
+++ BackEndPersona/src/main/java/sys/jp/persona/BackEndPersonaApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BackEndPersonaApplication {
@@ -19,6 +20,6 @@ public class BackEndPersonaApplication {
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("*").allowedHeaders("*");
 			}
-		}
+		};
 	}
 }
--- BackEndPersona/src/main/java/sys/jp/persona/rest/PersonaRest.java
+++ BackEndPersona/src/main/java/sys/jp/persona/rest/PersonaRest.java
@@ -1,5 +1,6 @@
 package sys.jp.persona.rest;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;

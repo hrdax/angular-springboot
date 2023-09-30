473f7cb6f0baeaf29e0b34cf583b3596ba203e4b
hrdax <juanpa_200018@hotmail.com>
2023-09-30 13:31:18 -0300

    persona service get and save

--- front_persona/src/app/services/persona/persona.service.ts
+++ front_persona/src/app/services/persona/persona.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PersonaService {
 
-  constructor() { }
+  private API_SERVER = "http://localhost:8081/personas/"
+
+  constructor(private httpClient: HttpClient) { }
+
+  public getAllPersonas(): Observable<any> {
+    return this.httpClient.get(this.API_SERVER);
+  }
+
+  public savePersona(persona:any): Observable<any> {
+    return this.httpClient.post(this.API_SERVER,persona);
+  }
 }
